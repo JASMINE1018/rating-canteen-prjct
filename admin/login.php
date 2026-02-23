993e0b9cfbff4230e4be6751e40f66c1a041c1ef
Jasmine Olivia Putri <jasminee18.jerukk@gmail.com>
2026-02-23 11:33:28 +0700

    Add login.php HTML

---
+++ admin/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+     <nav>
+        
+    </nav>
+    <header>
+    <h1><span>S</span>IGN UP | <span>D</span>AFTAR</h1>
+    <h5>"LEK GAK MLEBU DIANTEMI ATMIN"<h5>
+    </header>
+    <div class="form-0">
+        <div class="form">
+    <form method="POST">
+        <ul class="ul">
+            <li><label for="">Username: </label>
+            <li><input type="text" name="username" placeholder="Username" required></li>
+            <li><label for="">Email: </label>
+            <li><input type="email" name="email" placeholder="Email" required></li>
+            <li><label for="">Password: </label>
+            <li><input type="password" name="password" placeholder="Password" required></li>
+         </ul>
+         </div>
+     </div>
+    <button name="register">Daftar</button>
+</form>
+</body>
+</html>
